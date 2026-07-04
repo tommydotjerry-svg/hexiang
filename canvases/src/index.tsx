9277e6a6c9ab0b63ff5e186f9cdb5f47e8992fc9
tommydotjerry-svg <tommydotjerry@gmail.com>
2026-07-05 02:04:39 +0800

    Add files via upload

---
+++ canvases/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PaymentPRDWeb from '../payment-prd-web.canvas';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <PaymentPRDWeb />
+  </React.StrictMode>,
+);
