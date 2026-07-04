7104f6f0859d1ddf7238f1e78663144e076fa214
tommydotjerry-svg <tommydotjerry@gmail.com>
2026-07-05 02:12:48 +0800

    Delete canvases directory

--- canvases/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import PaymentPRDWeb from '../payment-prd-web.canvas';
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <PaymentPRDWeb />
-  </React.StrictMode>,
-);
